3cf32eb0106ffbccc2b3dbe42e012ac8a65481de
Shafin <82350431+blackipie@users.noreply.github.com>
2022-01-02 21:32:49 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,3 +37,20 @@ playPause.addEventListener("click", () => {
     playPause.querySelector(".play-btn").classList.toggle("hide");
   }
 });
+
+
+var popup = document.getElementById('popup-wrapper');
+var btn = document.getElementById("popup-btn");
+var span = document.getElementById("close");
+btn.onclick = function() {
+    popup.classList.add('show');
+}
+span.onclick = function() {
+    popup.classList.remove('show');
+}
+
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.classList.remove('show');
+    }
+}
